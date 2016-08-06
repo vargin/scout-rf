9c102ce9ca45c6b8dffa1a53011d4a71d0911103
Aleh Zasypkin <aleh.zasypkin@gmail.com>
2016-08-06 12:18:13 +0200

    Migrating to bitbang SPI.

---
+++ lib/halfduplexspi/halfduplexspi.h
@@ -0,0 +1,82 @@
+/* AVR half-duplex software SPI master
+ * @author: Ralph Doncaster 2015 public domain software
+ * connect 4.7K Ohm resistor between slave MO and MI pins and
+ * connect slave MOSI to AVR MO/MI
+ *  AVR              SLAVE
+ *  SCK ------------ SCK
+ *  MOMI --+-------- MOSI
+ *         +-\/\/\-- MISO
+ *            4.7K
+ *
+ * use spi_byte for tdd bi-directional spi transfer, or
+ * spi_in and spi_out for faster uni-directional transfer.
+ *
+ * define SPI_PORT and SPI_SCK before including this file
+ */
+
+
+#define cbi(x, y)    x &= ~(1 << y)
+#define sbi(x, y)    x |= (1 << y)
+
+#ifndef SPI_MOMI
+#define SPI_MOMI (SPI_SCK - 1)
+#endif
+
+#define SPI_DDR (*((&SPI_PORT) -1))
+#define SPI_PIN (*((&SPI_PORT) -2))
+
+void spi_setup() {
+  // Output mode.
+  sbi(SPI_DDR, SPI_SCK);
+}
+
+uint8_t spi_byte(uint8_t dataout) {
+  uint8_t datain, bits = 8;
+
+  do {
+    datain <<= 1;
+    if (SPI_PIN & (1 << SPI_MOMI)) datain++;
+
+    sbi (SPI_DDR, SPI_MOMI);        // output mode
+    if (dataout & 0x80) sbi (SPI_PORT, SPI_MOMI);
+    SPI_PIN = (1 << SPI_SCK);
+    cbi (SPI_DDR, SPI_MOMI);        // input mode
+    SPI_PIN = (1 << SPI_SCK);         // toggle SCK
+
+    cbi (SPI_PORT, SPI_MOMI);
+    dataout <<= 1;
+
+  } while (--bits);
+
+  return datain;
+}
+
+uint8_t spi_in(void) {
+  uint8_t pinstate, datain, bits = 8;
+
+  do {
+    datain <<= 1;
+    SPI_PIN = (1 << SPI_SCK);
+    pinstate = SPI_PIN;
+    SPI_PIN = (1 << SPI_SCK);         // toggle SCK
+    if (pinstate & (1 << SPI_MOMI)) datain++;
+  } while (--bits);
+
+  return datain;
+}
+
+void spi_out(uint8_t dataout) {
+  sbi (SPI_DDR, SPI_MOMI);        // output mode
+  uint8_t bits = 8;
+
+  do {
+    if (dataout & 0x80) SPI_PIN = (1 << SPI_MOMI);
+    SPI_PIN = (1 << SPI_SCK);
+    SPI_PIN = (1 << SPI_SCK);         // toggle SCK
+    cbi (SPI_PORT, SPI_MOMI);
+    dataout <<= 1;
+  } while (--bits);
+
+  cbi (SPI_DDR, SPI_MOMI);        // input mode
+}
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,64 @@
-#include <SPI85.h>
-#include <Mirf.h>
-#include <MirfHardwareSpi85Driver.h>
-#include "TinyDebugSerial.h"
+#include "uart.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include "utils.h"
 
-void setup()
-{
-  Serial.begin(115200);
+#define SPI_PORT PORTB
+#define SPI_SCK 2
+#define SPI_MOMI 0
 
-  Mirf.spi = &MirfHardwareSpi85;
-  Mirf.init();
+#include "halfduplexspi.h"
+
+const char data[14] = {"New iteration"};
+
+void debug(const char *str, bool newLine = true) {
+  while (*str) {
+    TxByte(*str++);
+  }
+
+  if (newLine) {
+    TxByte('\n');
+  }
 }
 
-void loop()
-{
-  uint8_t nrfStatus;
-  delay(3000);
-  Serial.print("\nMirf status: ");
-  nrfStatus = Mirf.getStatus();
-  // do back-to-back getStatus to test if CSN goes high
-  nrfStatus = Mirf.getStatus();
-  Serial.print(nrfStatus, HEX);
+void print2chars(uint16_t chars) {
+  TxByte(chars >> 8);
+  TxByte(chars & 0xff);
+}
+
+int main(void) {
+  // Setup outputs. Set port to HIGH to signify UART default condition.
+  DDRB |= _BV(DDB3);
+  PORTB |= _BV(PB3);
+
+  uint8_t result, address;
+  spi_setup();
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+  while (true) {
+    debug(data);
+
+    address = 0x0f;
+
+    do {
+      // discharge SCK->CSN RC
+      cbi(SPI_PORT, SPI_SCK);
+      print2chars(u8tohex(address));
+      debug(" ");
+
+      spi_byte(address);
+      result = spi_byte(address);
+
+      // charge SCK->CSN RC
+      sbi(SPI_PORT, SPI_SCK);
+
+      print2chars(u8tohex(result));
+
+      debug("\r\n");
+    } while (address--);
+
+    _delay_ms(1000);
+  }
+#pragma clang diagnostic pop
 }
